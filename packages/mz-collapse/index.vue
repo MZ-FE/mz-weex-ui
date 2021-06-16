304152de19a587abf0e18813c4d5cf05ce943784
冼永恒 <xianyh1@midea.com>
2021-06-16 16:04:41 +0800

    feat: 折叠面板优化

--- packages/mz-collapse/index.vue
+++ packages/mz-collapse/index.vue
@@ -19,76 +19,76 @@
 </template>
 
 <script>
-import { ic_moreUp } from './assets/icon-base64';
+  import { arrowIconNew } from '../setting/icon.base64';
 
-export default {
-  name: 'mz-collapse',
-  props: {
-    /**
-     * 是否禁用，禁用后不可下拉
-     */
-    disabled: {
-      type: Boolean,
-      default: false
-    }
-  },
-  components: {},
-  data () {
-    return {
-      isExpand: false, // 是否展开折叠面板
-      arrowImg: ic_moreUp
-    };
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    /**
-     * 下拉展示内容
-     */
-    toggleExpand () {
-      // 禁用下拉判断
-      if (this.disabled) {
-        return;
+  export default {
+    name: 'mz-collapse',
+    props: {
+      /**
+       * 是否禁用，禁用后不可下拉
+       */
+      disabled: {
+        type: Boolean,
+        default: false
       }
-      this.isExpand = !this.isExpand;
-    }
-  },
+    },
+    components: {},
+    data () {
+      return {
+        isExpand: false, // 是否展开折叠面板
+        arrowImg: arrowIconNew
+      };
+    },
+    computed: {},
+    watch: {},
+    methods: {
+      /**
+       * 下拉展示内容
+       */
+      toggleExpand () {
+        // 禁用下拉判断
+        if (this.disabled) {
+          return;
+        }
+        this.isExpand = !this.isExpand;
+      }
+    },
 
-  created () {},
+    created () {},
 
-  mounted () {},
+    mounted () {},
 
-  destroyed () {}
-};
+    destroyed () {}
+  };
 </script>
 
 <style scoped>
-.mz-collapse {
-  background-color: #fff;
-}
-.mz-collapse-header {
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: stretch;
-}
+  .mz-collapse {
+    background-color: #fff;
+  }
+  .mz-collapse-header {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: stretch;
+  }
 
-.arrow-wrapper {
-  padding-right: 32px;
-  flex-direction: row;
-  align-items: center;
-}
-.icon-arrow {
-  width: 32px;
-  height: 16px;
-  transform: rotate(-90deg);
-  transition: transform ease-in;
-}
+  .arrow-wrapper {
+    padding-right: 32px;
+    flex-direction: row;
+    align-items: center;
+  }
+  .icon-arrow {
+    width: 22px;
+    height: 22px;
+    transform: rotate(0);
+    transition: transform ease-in;
+  }
 
-.icon-arrow-expand {
-  transform: rotate(0);
-}
+  .icon-arrow-expand {
+    transform: rotate(90deg);
+  }
 
-.mz-collapse-content {
-  padding-left: 20px;
-}
+  .mz-collapse-content {
+    padding-left: 20px;
+  }
 </style>
